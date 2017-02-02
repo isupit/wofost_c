59a576d13fd3f9693f221fd7191352e1996179d1
Iwan Supit <iwan.supit@wur.nl>
2017-02-02 17:03:53 +0100

    get soildata

---
+++ soil.h
@@ -0,0 +1,23 @@
+char *Parameter[]={"SMW",
+                   "SMFCF",
+		   "SM0",
+		   "CRAIRC",
+		   "K0",
+		   "SOPE",
+		   "KSUB",
+		   "SPADS",
+                   "SPODS",
+		   "SPASS",
+		   "SPOSS",
+		   "DEFLIM",
+		   "NULL"
+		   };
+		   
+char *Parameter2[]={
+                    "SMTAB",
+                    "CONTAB",
+                    "NULL"
+		    };
+
+extern int FillVariables();
+extern int FillAfgenTables();
---
+++ soildata.c
@@ -0,0 +1,82 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "wofost.h"
+#include "soil.h"
+
+
+int GetSoilData()
+{
+  AFGEN *FillData[2], *Table, *start;
+  int i, c;
+  float Variable[100], XValue, YValue;
+  char x[2], xx[2],  word[100];
+  FILE *fq;
+
+ if ((fq = fopen("data//ec1.new", "rt")) == NULL)
+    {fprintf(stderr, "Cannot open input file.\n"); return 0;}
+
+ i=0;
+  while ((c=fscanf(fq,"%s",word)) != EOF) 
+  {
+    if (!strcmp(word, Parameter[i])) {
+        while ((c=fgetc(fq)) !='=');
+	fscanf(fq,"%f",  &Variable[i]);
+
+	i++; 
+       }  
+
+  }
+
+  if (i!=NUMBER_OF_VARIABLES) return 0;
+  rewind(fq);  
+  FillVariables(Variable);
+ 
+
+  i=0;
+  while ((c=fscanf(fq,"%s",word)) != EOF) 
+  {
+    if (!strcmp(word, Parameter2[i])) {
+        Table = start = malloc(sizeof(AFGEN));
+	fscanf(fq,"%s %f %s  %f", x, &Table->x, xx, &Table->y);
+        Table->next = NULL;				     
+			       
+	while ((c=fgetc(fq)) !='\n');
+	while (fscanf(fq," %f %s  %f",  &XValue, xx, 
+&YValue) > 0)
+ {
+	    
+	    Table->next = malloc(sizeof(AFGEN));
+            Table = Table->next; Table->next = NULL;
+	    Table->x = XValue;
+	    Table->y = YValue;
+	    
+	    while ((c=fgetc(fq)) !='\n');
+	    }
+	    FillData[i] = start;
+	i++; 
+       }      
+  }
+
+  if (i!= NUMBER_OF_TABLES) return 0;
+ 
+  
+  DeltaTempSum         = FillData[0];
+  SpecificLeaveArea    = FillData[1];
+  SpecificStemArea     = FillData[2];
+  KDiffuseTb           = FillData[3];
+  EFFTb                = FillData[4];
+  MaxAssimRate         = FillData[5];
+  FactorAssimRateTemp  = FillData[6];
+  FactorGrossAssimTemp = FillData[7];
+  FactorSenescence     = FillData[8];
+  Roots                = FillData[9];
+  Leaves               = FillData[10];
+  Stems                = FillData[11];
+  Storage              = FillData[12];
+  DeathRateStems       = FillData[13];
+  DeathRateRoots       = FillData[14]; 
+
+return 1;
+}
+
