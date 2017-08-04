caadbbde9fac5217f25765f5eaf77d271a4a428a
Iwan Supit <iwan.supit@wur.nl>
2017-08-04 16:53:57 +0200

    2017-08-04

--- InitializeCrop.c
+++ InitializeCrop.c
@@ -23,6 +23,7 @@ void InitializeCrop(int Emergence)
                     Temp-TempBaseEmergence);
 	     TempSum += DeltaTempSum;
 	     Day++;
+             simTime.tm_mday++;
 	     }
 	}	     
        
